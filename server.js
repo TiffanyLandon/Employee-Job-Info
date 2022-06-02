e02c8f7362795c4c7abb17a2c6af525f3abd6f22
Tiffany Bryan <tiffanym.bryan@gmail.com>
2022-06-02 07:38:34 -0400

    start project

--- server.js
+++ server.js
@@ -2,16 +2,23 @@ const express = require('express');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mysql = require('mysql2');
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+// Connect to database
+app.get('/',(req,res) => {
+    res.json({
+        message: 'landon brush your teeth'
+    });
+});
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
-  res.status(404).end();
-});
+    res.status(404).end();
+  });
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
