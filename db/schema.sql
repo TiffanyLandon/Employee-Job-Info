ef97ffc94cce78ad89b34f42b9d2a90bb1c533a5
Tiffany Bryan <tiffanym.bryan@gmail.com>
2022-06-02 15:24:02 -0400

    create employee

--- db/schema.sql
+++ db/schema.sql
@@ -6,11 +6,20 @@ CREATE TABLE department (
  name VARCHAR(30) NOT NULL
 );
 
+CREATE TABLE employees (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INTEGER,
+  manager_id INTEGER
+);
+
 CREATE TABLE role (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(50) NOT NULL,
   salary  DECIMAL(30,2) NOT NULL,
   department_id INTEGER,
 
-  CONSTRAINT fk_department FOREIGN KEY (department_id)REFERENCES department(id)ON DELETE SET NULL
+ FOREIGN KEY (department_id)REFERENCES department(id)ON DELETE SET NULL
 );
+
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,9 @@
+
+INSERT INTO employees (first_name, last_name,role_id,manager_id)
+VALUES
+  ('Tanya','Longhorn',1,1),
+  ('Peter','John',2,Null);
+  
 INSERT INTO role (title,salary,department_id)
 VALUES
   ('Customer Service',15.50,1),
@@ -7,4 +13,6 @@ INSERT INTO department (name)
 VALUES
   ('Member Services'),
   ('Services');
+
+  
  
