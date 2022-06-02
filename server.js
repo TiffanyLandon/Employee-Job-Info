284bf137c226f4091b68303d3a76ebc7331af80d
Tiffany Bryan <tiffanym.bryan@gmail.com>
2022-06-02 09:19:13 -0400

    update the get all candidates

--- server.js
+++ server.js
@@ -11,53 +11,65 @@ app.use(express.json());
 // Connect to database
 const db = mysql.createConnection(
     {
-      host: 'localhost',
-      // Your MySQL username,
-      user: 'root',
-      // Your MySQL password
-      password: 'landon12!',
-      database: 'department'
+        host: 'localhost',
+        // Your MySQL username,
+        user: 'root',
+        // Your MySQL password
+        password: 'landon12!',
+        database: 'department'
     },
     console.log('Connected to the department database.')
-  );
+);
 
-  db.query(`SELECT * FROM department`, (err, rows) => {
-    console.log(rows);
-  });
+// Get all candidates
+app.get('/api/department', (req, res) => {
+    const sql = `SELECT * FROM department`;
+
+    db.query(sql, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
+});
 
 
-  // GET a single department 
+// GET a single department 
 db.query(`SELECT * FROM department WHERE id = 1`, (err, row) => {
     if (err) {
-      console.log(err);
+        console.log(err);
     }
     console.log(row);
-  });
+});
 
-  // Delete a department
+// Delete a department
 db.query(`DELETE FROM department WHERE id = ?`, 1, (err, result) => {
     if (err) {
-      console.log(err);
+        console.log(err);
     }
     console.log(result);
-  });
+});
 
-  // Create a candidate
+// Create a candidate
 const sql = `INSERT INTO department (id, department_name) 
 VALUES (?,?)`;
-const params = [1,'Pickup'];
+const params = [1, 'Pickup'];
 
 db.query(sql, params, (err, result) => {
-if (err) {
-console.log(err);
-}
-console.log(result);
+    if (err) {
+        console.log(err);
+    }
+    console.log(result);
 });
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
     res.status(404).end();
-  });
+});
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
